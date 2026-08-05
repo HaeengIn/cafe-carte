fd8034e4b3c6f027064e43208bbdf20b0107567c
HaeengIn <haeengin@gmail.com>
2026-08-06 01:05:31 +0900

    Add context, members router

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 
+from routers.members import members_router
+
 from template_config import templates
 
 app = FastAPI(redirect_slashes=True)
@@ -12,8 +14,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 async def index(request: Request):
     title = "CAFE CARTE - Unofficial Fan Website"
 
-    context = {"title": title}
+    context = {
+        "title": title,
+    }
 
     return templates.TemplateResponse(
         request=request, context=context, name="index.html"
     )
+
+
+app.include_router(members_router)
