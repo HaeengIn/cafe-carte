b55dcc776512db764b0e9fe02e40f6a9ef4f7d60
HaeengIn <haeengin@gmail.com>
2026-08-10 21:12:57 +0900

    Fix warning passage

--- routers/about.py
+++ routers/about.py
@@ -10,7 +10,7 @@ async def index(request: Request):
     title = "ABOUT - Cafe Carte"
 
     warning = (
-        '본 웹 사이트는 <a href="https://samg.net" target="_blank" rel="noopener noreferrer">SAMG Entertainment</a>의 브랜드인 TWILLIT STUDIO의 3D 버츄얼 스트리머/유튜버 그룹인 \'카페 카르테\'의 <b>비공식 팬 웹 사이트</b>입니다.<br>'
+        '본 웹 사이트는 <a href="https://samg.net" target="_blank" rel="noopener noreferrer">SAMG Entertainment</a>의 브랜드인 TWILLIT STUDIO의 3D 버츄얼 스트리머/유튜버 그룹, \'카페 카르테\'의 <b>비공식 팬 웹 사이트</b>입니다.<br>'
         "SAMG Entertainment 또는 TWILLIT STUDIO의 허가없이 제작된 웹 사이트이며, 사전 공지 없이 언제든 삭제될 수 있습니다."
     )
     developer_username = "HaeengIn"
