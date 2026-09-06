a123cb744590e626d18149816eabe4403dff7f50
HaeengIn <haeengin@gmail.com>
2026-09-07 00:25:48 +0900

    Fix scroll animation

--- static/js/scroll-animation.js
+++ static/js/scroll-animation.js
@@ -1,16 +1,19 @@
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('is-visible');
-            observer.unobserve(entry.target);
-        }
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+        observer.unobserve(entry.target);
+      }
     });
-}, {
-    threshold: 0.5
-});
+  },
+  {
+    threshold: 0.5,
+  },
+);
 
-document.querySelectorAll(
-    '.fade-down, .fade-left, .fade-right'
-).forEach(element => {
+document
+  .querySelectorAll(".fade-down, .fade-left, .fade-right")
+  .forEach((element) => {
     observer.observe(element);
-});
+  });
